26f7989b80bdfe893c80e000223c3b4eaeb5c360
Sean Brackenborough <sean.brackenborough@sysmech.co.uk>
2023-12-01 20:29:36 +0000

    day1

--- AdventOfCode2023/day1/solution.py
+++ AdventOfCode2023/day1/solution.py
@@ -13,20 +13,12 @@ SPELT_NUMBERS = [
 
 def get_line_value(line: str) -> int:
     line_numbers = "".join([c for c in line if c.isdigit()])
-    print("adding", int(line_numbers[0] + line_numbers[-1]))
     return int(line_numbers[0] + line_numbers[-1])
 
 
-def convert_spelt_numbers(
-    line: str,
-) -> (
-    str
-):  # ! this does not work, because spelt numbers can overlap, e.g eightwothree should be -> 8wo3, but this produces eigh23
-    print(line)
+def convert_spelt_numbers(line: str) -> str:
     for sn in SPELT_NUMBERS:
-        line = line.replace(sn, str(SPELT_NUMBERS.index(sn) + 1))
-    print(line)
-    print("=" * 20)
+        line = line.replace(sn, f"{sn}{SPELT_NUMBERS.index(sn) + 1}{sn}")
     return line
 
 
