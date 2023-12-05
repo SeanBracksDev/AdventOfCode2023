f560dc22d02f5172da3859c5d76a35c343854d6d
Sean Brackenborough <sean.brackenborough@sysmech.co.uk>
2023-12-05 18:39:48 +0000

    Day 4 part 2 done

--- AdventOfCode2023/day4/solution.py
+++ AdventOfCode2023/day4/solution.py
@@ -1,30 +1,59 @@
-def _parse(data: list[str]) -> list[list[list[str]]]:
-    parsed_data: list[list[list[str]]] = []
+from functools import lru_cache
+
+
+def _parse(data: list[str]) -> dict[int, list[list[str]]]:
+    parsed_data: dict[int, list[list[str]]] = {}
     for line in data:
+        card_id = int(line.split(":")[0].split()[1])
         winning_section, got_section = line.split(":")[1].split("|")
         winning_numbers = winning_section.strip().split()
         got_numbers = got_section.strip().split()
-        parsed_data.append([winning_numbers, got_numbers])
+        parsed_data[card_id] = [winning_numbers, got_numbers]
     return parsed_data
 
 
 def part1(_input: list[str]) -> int:
     total = 0
-    for winning_numbers, got_nummbers in _parse(_input):
-        count = 0
-        for number in got_nummbers:
-            if number in winning_numbers:
-                count += 1
-        if count:
-            if count == 1:
+    for winning_numbers, got_numbers in _parse(_input).values():
+        matching_numbers = [num for num in got_numbers if num in winning_numbers]
+        if len(matching_numbers):
+            if len(matching_numbers) == 1:
                 total += 1
             else:
-                total += 2 ** (count - 1)
+                total += 2 ** (len(matching_numbers) - 1)
     return total
 
 
+def _get_won_games(
+    card_id: int, game_data: dict[int, list[list[str]]], total_cards: int, cache=None
+) -> int:
+    if cache is None:
+        cache = {}
+
+    if (card_id, total_cards) in cache:
+        return cache[(card_id, total_cards)]
+
+    winning_numbers, got_numbers = game_data[card_id]
+    matching_numbers = [num for num in got_numbers if num in winning_numbers]
+
+    additional_cards = len(matching_numbers)
+
+    for ci in range(card_id + 1, min(card_id + additional_cards + 1, len(game_data))):
+        additional_cards += _get_won_games(ci, game_data, 0, cache)
+
+    cache[(card_id, total_cards)] = additional_cards
+
+    return additional_cards
+
+
 def part2(_input: list[str]) -> int:
-    ...
+    parsed_data = _parse(_input)
+    total_cards = len(parsed_data)
+
+    for card_id in parsed_data:
+        total_cards += _get_won_games(card_id, parsed_data, total_cards)
+
+    return total_cards
 
 
 parts = (part1, part2)
