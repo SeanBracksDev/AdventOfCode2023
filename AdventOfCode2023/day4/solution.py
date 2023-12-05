faf82ab68d42882bdbc17427365149c778be0f65
Sean Brackenborough <sean.brackenborough@sysmech.co.uk>
2023-12-05 13:57:04 +0000

    Day 3 init setup

---
+++ AdventOfCode2023/day4/solution.py
@@ -0,0 +1,13 @@
+def _parse(data: list[str]) -> list[list[str]]:
+    ...
+
+
+def part1(_input: list[str]) -> int:
+    ...
+
+
+def part2(_input: list[str]) -> int:
+    ...
+
+
+parts = (part1, part2)
